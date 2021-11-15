d0805d25de73c312e1ebd341423e588e369708cf
MichaelPHartmann <mph101mph@gmail.com>
2021-11-15 16:18:03 +0200

    Made db methods symetrical for current and watchlist databases

--- database/access.py
+++ database/access.py
@@ -3,39 +3,73 @@ import sys
 import sqlite3
 
 class dbAccess():
-    """The parent function for all database operations."""
+    """The parent class for all database operations.
+    This class contains standard methods for all databases"""
+
     def __init__(self):
         # The current database will always be named the same
         self.current_db = 'current.db'
         # Need to support multiple watchlist databases so the '%n' can be replaced with an arbitrary string
         self.watchlist_db_base = '%nwatchlist.db'
 
+
+class currentDB(dbAccess):
+    """A child class of dbAccess that interacts with the current stock holdings database."""
+    def __init__(self):
+        # Import parent class attributes
+        dbAccess.__init__(self)
+        # Create a connection with the database
+        self.conn = sqlite3.connect(self.current_db)
+        # Create a cursor object
+        self.c = self.conn.cursor()
+
     def _setup_current_db(self):
-        already_exists = os.path.isfile(self.current_db)
-        conn = sqlite3.connect(self.current_db)
-        conn.execute("""CREATE TABLE current
+        """Sets up a database for current stock holdings.
+        Only to be used on a fresh current file!"""
+        self.conn.execute("""CREATE TABLE current
         (ID INT PRIMARY KEY  NOT NULL,
         TICKER TEXT NOT NULL,
         PRICE REAL NOT NULL);
         """)
 
     def add_to_current_db(self, id, ticker, price):
-        conn = sqlite3.connect(self.current_db)
-        c = conn.cursor()
         insert = (id, ticker, price)
-        c.execute(f"INSERT INTO current VALUES (?,?,?)", insert);
-        conn.commit()
+        self.c.execute(f"INSERT INTO current VALUES (?,?,?)", insert);
+        self.conn.commit()
 
     def see_current_db(self):
-        conn = sqlite3.connect(self.current_db)
-        c = conn.cursor()
-        for row in c.execute('SELECT * FROM current'):
+        for row in self.c.execute('SELECT * FROM current'):
             print(row)
 
-    def _setup_watchlist_db(self):
-        pass
 
-d = dbAccess()
-# d._setup_current_db()
-# d.add_to_current_db(3,'FARM',150)
-d.see_current_db()
+class watchlistDB(dbAccess):
+    """A child class of dbAccess that interacts with the current stock holdings database."""
+    def __init__(self, nickname):
+        # Import parent class attributes
+        dbAccess.__init__(self)
+        self.nickname = nickname
+        self.watchlist_db = nickname + self.watchlist_db_base
+        # Create a connection with the database
+        self.conn = sqlite3.connect(self.watchlist_db)
+        # Create a cursor object
+        self.c = self.conn.cursor()
+        # A tuple is recomended as passing it in prevents SQL injection attacks
+        self.nickname_tuple = (self.nickname,)
+
+    def _setup_watchlist_db(self):
+        """Sets up a database for current stock holdings.
+        Only to be used on a fresh current file!"""
+        self.conn.execute("""CREATE TABLE ?
+        (ID INT PRIMARY KEY  NOT NULL,
+        TICKER TEXT NOT NULL,
+        PRICE REAL NOT NULL);
+        """, self.nickname_tuple)
+
+    def add_to_watchlist_db(self, id, ticker, price):
+        insert = (id, ticker, price)
+        self.c.execute(f"INSERT INTO current VALUES (?,?,?)", insert);
+        self.conn.commit()
+
+    def see_watchlist_db(self):
+        for row in self.c.execute('SELECT * FROM ?', self.nickname_tuple):
+            print(row)
